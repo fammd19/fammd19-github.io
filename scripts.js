4a0372453ad915197fd0517949e3c7e72f9d4db0
Fi Mellink <fimellink@gmail.com>
2023-11-27 11:22:03 +1100

    Add icons, create blog page and amend style sheets

--- scripts.js
+++
@@ -1,7 +0,0 @@
-$(".ahashlink").click( function() {
-
-    var location = $(this).attr("href");
-    var offset = $(location).offset().top;
-    $("body").scrollTop(offset+100px);
-    return false;
-});
